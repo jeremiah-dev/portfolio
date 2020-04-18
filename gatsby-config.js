498adc4d4448a70fee1f64b7c5ab8e3ea50b215a
Jeremiah Walter <jeremiah.walter@rxpservices.com>
2020-04-18 14:34:17 +1000

    Updated site favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,9 +18,9 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-      {
-        // https://www.gatsbyjs.org/docs/add-a-manifest-file/#using-gatsby-plugin-manifest
-        // better google results for progressive web apps:
+    {
+      // https://www.gatsbyjs.org/docs/add-a-manifest-file/#using-gatsby-plugin-manifest
+      // better google results for progressive web apps:
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
@@ -29,12 +29,12 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/Avatar.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    `gatsby-plugin-styled-components`
+    `gatsby-plugin-styled-components`,
   ],
 }
