fa17a753a9089322f5c2834f37ecf8a9c0120f7d
Jeremiah Walter <jeremiah.walter@rxpservices.com>
2020-04-20 23:03:02 +1000

    404 page SEO fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,12 +3,27 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = () => (
+const NotFoundPage = ({ data }) => (
   <Layout>
-    <SEO title="404: Not found" />
+    <SEO
+      title="404: Not found"
+      image={data.SocialMediaPreview.childImageSharp.fixed}
+    />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    SocialMediaPreview: file(relativePath: { eq: "images/og-image.png" }) {
+      childImageSharp {
+        fixed(width: 1200, height: 630, quality: 100) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
 
 export default NotFoundPage
